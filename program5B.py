53af9b8b9c3f74f842da309c58740ff9c989ab43
matthewdylewski <92966564+matthewdylewski@users.noreply.github.com>
2024-11-11 12:37:36 -0500

    Update program5B.py

--- program5B.py
+++ program5B.py
@@ -45,9 +45,9 @@ def program5B(n: int, W: int, heights: List[int], widths: List[int]) -> Tuple[in
 
 
 if __name__ == '__main__':
-    # n, W = map(int, input().split())
-    # heights = list(map(int, input().split()))
-    # widths = list(map(int, input().split()))
+    n, W = map(int, input().split())
+    heights = list(map(int, input().split()))
+    widths = list(map(int, input().split()))
     m, total_height, num_paintings = program5B(n, W, heights, widths)
     print(m)
     print(total_height)
